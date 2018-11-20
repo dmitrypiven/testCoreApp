a8cd31b8fe2cb30a3e17ac5d467d8bc7f8593a28
dmitrypiven <dpiven@holbi.co.uk>
2018-11-20 22:02:44 +0200

    Add project files.

---
+++ testCoreApp/Models/MockPieRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace testCoreApp.Models
+{
+    public class MockPieRepository : IPieRepository
+    {
+        private List<Pie> _pies;
+        public MockPieRepository()
+        {
+            if (_pies == null)
+            {
+                InitializePages();
+            }
+        }
+
+        private void InitializePages()
+        {
+            _pies = new List<Pie>();
+            _pies.Add(new Pie() { Id = 1, Name = "Apple Pie",Price = 12.95M,ShortDescription = "Apple Pie", LongDescription = "Apple Pie", ImageThumbnailUrl = "",ImageUrl = "",IsPieOfTheWeek = false });
+            _pies.Add(new Pie() { Id = 2, Name = "Blueberry Cheese Cake", Price = 18.95M, ShortDescription = "Blueberry Cheese Cake", LongDescription = "Blueberry Cheese Cake", ImageThumbnailUrl = "", ImageUrl = "", IsPieOfTheWeek = false });
+            _pies.Add(new Pie() { Id = 3, Name = "Cheese Cake", Price = 16.95M, ShortDescription = "Cheese Cake", LongDescription = "Cheese Cake", ImageThumbnailUrl = "", ImageUrl = "", IsPieOfTheWeek = false });
+            _pies.Add(new Pie() { Id = 4, Name = "Cherry Pie", Price = 11.95M, ShortDescription = "Cherry Pie", LongDescription = "Cherry Pie", ImageThumbnailUrl = "", ImageUrl = "", IsPieOfTheWeek = false });
+        }
+
+        public IEnumerable<Pie> GetAllPies()
+        {
+            return _pies;
+        }
+
+        public Pie GetPieById(int pieId)
+        {
+            return _pies.FirstOrDefault(arg=>arg.Id == pieId);
+        }
+    }
+}
